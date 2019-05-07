2f83228c4732103149db31e59aac9398ec15e3dc
Jonathan Trujillo <jonathantruest@gmail.com>
2019-05-07 21:21:19 +0100

    Victory condition implemented. Collisions complete. Working on modal.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    this.x = -1;
+    this.x = 0;
     this.y = 150;
     this.speed = 600;
     this.road = [50, 150, 220];
@@ -10,13 +10,13 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    /**if(this.x < 900)
+    if(this.x < 900)
         this.x += this.speed * dt;
     else {
         this.y = this.road[Math.floor(Math.random() * this.road.length)];
         this.speed = Math.floor(Math.random() * (50 - 600)) + 600;
         this.x = -200;
-    }*/
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,11 +30,11 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.x = 200;
     this.y = 400;
+    this.road = [50, 150, 220, 300, 400];
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(dt) {
-    console.log("update player");
     this.handleCollision();
 };
 
@@ -45,8 +45,7 @@ Player.prototype.render = function() {
 
 Player.prototype.handleCollision = function(key) {
     for(enemy of allEnemies){
-        if(enemy.x === this.x && enemy.y === this.y){
-            console.log("collision");
+        if(enemy.y === this.y && (this.x >= enemy.x - 50 && this.x <= enemy.x + 50)){
             this.x = 200;
             this.y = 400;
         }
@@ -58,18 +57,36 @@ Player.prototype.handleInput = function(key) {
         this.x -= 100;
     } else if(key === 'right' && this.x < 400){
         this.x += 100;
-    } if(key === 'up' && this.y > 0){
-        this.y -= 100;
-    } if(key === 'down' && this.y < 400){
-        this.y += 100;
+    } else if(key === 'up' && this.y > this.road[0]){
+        let index = 1;
+        let found = false;
+        while(index < this.road.length && !found){
+            if(this.y === this.road[index]){
+                this.y = this.road[index-1];
+                found = true;
+            }
+            index++;
+        }
+    } else if(key === 'down' && this.y < 400){
+        let index = 0;
+        let found = false;
+        while(index < this.road.length && !found){
+            if(this.y === this.road[index]){
+                this.y = this.road[index+1];
+                found = true;
+            }
+            index++;
+        }
+    } else if(key === 'up' && this.y === this.road[0]){
+        console.log("victory");
     }
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-//let allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
-let allEnemies = [new Enemy()];
+let allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+//let allEnemies = [new Enemy()];
 let player = new Player();
 
 
@@ -85,3 +102,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//document.getElementById("victory-modal").style.display = "block";
